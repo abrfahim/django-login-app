cf7236aad6a519dc529f440bc1d4f5b139dd0d14
Abrar Fahim <engr.abrarfahim36@gmail.com>
2023-11-03 19:41:07 +0600

    project uploaded

---
+++ mysecond/login_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    fb_id = models.URLField(blank=True)
+    fb_pic = models.ImageField(upload_to='profile_pic', blank=True)
+    
+    def __str__(self):
+        return self.user.username
